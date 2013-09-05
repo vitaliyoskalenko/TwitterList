051332c483928fc20a8bd9e36658124c482fb87d
Oskalenko Vitaliy <oskalenkoVit@ukr.net>
2013-09-05 10:32:14 +0300

    initial

--- gen/com/voskalenko/twitterlist/R.java
+++ gen/com/voskalenko/twitterlist/R.java
@@ -25,7 +25,8 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_list=0x7f030000;
-        public static final int list_item=0x7f030001;
+        public static final int list_header=0x7f030001;
+        public static final int list_item=0x7f030002;
     }
     public static final class menu {
         public static final int activity_list_menu=0x7f060000;
--- src/com/voskalenko/twitterlist/ui/TwitterListAdapter.java
+++ src/com/voskalenko/twitterlist/ui/TwitterListAdapter.java
@@ -28,162 +28,180 @@ import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class TwitterListAdapter extends BaseAdapter implements Filterable{
+public class TwitterListAdapter extends BaseAdapter implements Filterable {
 
-		private static final String TAG = TwitterListAdapter.class.getSimpleName(); 
-		
-	    private  List<TwitterObj> twitterLst;
-	    private LayoutInflater mInflater;
-	    private SimpleDateFormat formater;
-	    
-	    private final Filter filter = new Filter() {
-	    	
-	    	@Override
-	    	protected FilterResults performFiltering(CharSequence condition) {
-	    		FilterResults results = new FilterResults();
-	    		DatabaseManager dbMng = DatabaseManager.getInstance();
-	    		List<TwitterObj> obj;
-	    		
-	    		if(!TextUtils.isEmpty(condition))
-	    			obj = dbMng.getTwitterHomeLines((String)condition);
-	    		else 
-	    			obj = dbMng.getAllTwitterHomeLines();	
-	    		
-	    		results.values = obj; 
-	    		results.count = obj.size();
-	    		
-	    		return results;
-	    	}
-	    	
-	    	@Override
-	    	protected void publishResults(CharSequence condition,
-	    			FilterResults results) {
-	    		if(results != null) {
-	    			twitterLst = (List<TwitterObj>) results.values;
-	    			notifyDataSetChanged();
-	    		}
-	    	}
-	    	
-	    };
-	    
-	    public TwitterListAdapter(Context ctx, List<TwitterObj> lst)
-	    {
-	    	twitterLst = lst;
-	        mInflater = LayoutInflater.from(ctx);
-	        formater = new SimpleDateFormat("EEEE MMMM dd", Locale.US);
-	    }
-    	
-	    @Override
-	    public Filter getFilter() {
-	    	return filter;
-	    }
-	    
-	    public int getCount()
-	    {
-	        return twitterLst == null ? 0 : twitterLst.size();
-	    }
+	private static final String TAG = TwitterListAdapter.class.getSimpleName();
+	private static final int TYPE_GROUP_HEADER = 0;
+	private static final int TYPE_ITEM = 1;
+	private static final int ITEM_TYPE_COUNT = 2;
 
-	    public Object getItem(int position)
-	    {
-	        return twitterLst == null ? null : twitterLst.get(position);
-	    }
+	private List<TwitterObj> twitterLst;
+	private LayoutInflater mInflater;
+	private SimpleDateFormat formater;
 
-	    public long getItemId(int position)
-	    {
-	        return position;
-	    }
+	private final Filter filter = new Filter() {
 
-	    public View getView(int position, View convertView, ViewGroup parent)
-	    {
-	        UiHolder holder;
+		@Override
+		protected FilterResults performFiltering(CharSequence condition) {
+			FilterResults results = new FilterResults();
+			DatabaseManager dbMng = DatabaseManager.getInstance();
+			List<TwitterObj> obj;
 
-	        if(convertView == null)
-	        {
-	            convertView = mInflater.inflate(R.layout.list_item, null);
-	            
-	            holder = new UiHolder();
-	            holder.imgPhoto = (ImageView) convertView.findViewById(R.id.img_photo);
-	            holder.txtUser = (TextView) convertView.findViewById(R.id.txt_user);
-	            holder.txtCreatedAt = (TextView) convertView.findViewById(R.id.txt_created_at);
-	            holder.txtDescription = (TextView) convertView.findViewById(R.id.txt_description);
-	            holder.txtHeader = (TextView) convertView.findViewById(R.id.txt_header);
+			if (!TextUtils.isEmpty(condition))
+				obj = dbMng.getTwitterHomeLines((String) condition);
+			else
+				obj = dbMng.getAllTwitterHomeLines();
 
-	            convertView.setTag(holder);
-	        } else {
-	            holder = (UiHolder) convertView.getTag();
-	        }
-	        
-	        String url = twitterLst.get(position).getUser().getProfileImageURL();
-	        new DownloadPhotoTask(holder.imgPhoto).execute(url); 
-	        
-	        holder.txtUser.setText(twitterLst.get(position).getUser().getName());
-	        holder.txtCreatedAt.setText(twitterLst.get(position).getCreatedAt().getTime().toString());
-	        holder.txtDescription.setText(twitterLst.get(position).getText());
-	       
-	        
-	        Calendar prevDate = twitterLst.get(position - 1 < 0 ? 0 : position - 1).getCreatedAt();
-	        Calendar thisDate = twitterLst.get(position).getCreatedAt();
-	        
-	        if(prevDate.get(Calendar.DAY_OF_MONTH) ==  thisDate.get(Calendar.DAY_OF_MONTH) && position > 0)
-	        	holder.txtHeader.setVisibility(View.GONE);  
-	        else {
-	        		holder.txtHeader.setText(formater.format(thisDate.getTime()));
-	        		holder.txtHeader.setVisibility(View.VISIBLE);
-	        	}
-	        
-	        return convertView;
-	    }
+			results.values = obj;
+			results.count = obj.size();
 
-	    static class UiHolder
-	    {
-	        ImageView imgPhoto;
-	        TextView txtUser;
-	        TextView txtCreatedAt;
-	        TextView txtDescription;
-	        TextView txtHeader;
-	    }
-	    
-	    public class DownloadPhotoTask extends AsyncTask<String, Bitmap, Bitmap> {
-	    	
-	    	private final WeakReference<ImageView> imgPhotoRef;
+			return results;
+		}
 
-	    	public DownloadPhotoTask(ImageView imgPhoto) {
-				imgPhotoRef = new WeakReference<ImageView>(imgPhoto);
+		@Override
+		protected void publishResults(CharSequence condition,
+				FilterResults results) {
+			if (results != null) {
+				twitterLst = (List<TwitterObj>) results.values;
+				notifyDataSetChanged();
 			}
-	    	
-	    	@Override
-	    	protected void onPreExecute() {
-	    		setPhoto(null);
-	    		super.onPreExecute();
-	    	}
-	    	
-	        @Override
-	        protected Bitmap doInBackground(String... url) {
-	        	URL urlPhoto = null;
-	        	Bitmap bmp = null;
-				try {
-					urlPhoto = new URL(url[0]);
-					bmp = BitmapFactory.decodeStream(urlPhoto.openConnection().getInputStream());
-				} catch (MalformedURLException e) {
-					Log.e(TAG, "Failed to get photo");
-				} catch (IOException e) {
-					Log.e(TAG, "Failed to get photo");
-				}	
-				return bmp;
-	        }
+		}
 
-	        @Override
-	        protected void onPostExecute(Bitmap bmp) {
-	        	setPhoto(bmp);
-	            
-	        }
-	        
-	       private void setPhoto(Bitmap bmp) {
-	                final ImageView imgPhoto = imgPhotoRef.get();
-	                if (imgPhoto != null) {
-	                	imgPhoto.setImageBitmap(bmp);
-	            }
-	       }
-	    }
+	};
 
+	public int getViewTypeCount() {
+		return ITEM_TYPE_COUNT;
+	};
+	
+	public int getItemViewType(int position) {
+		Calendar prevDate = twitterLst.get(position - 1 < 0 ? 0 : position - 1)
+				.getCreatedAt();
+		Calendar thisDate = twitterLst.get(position).getCreatedAt();
+
+		return prevDate.get(Calendar.DAY_OF_MONTH) == thisDate
+				.get(Calendar.DAY_OF_MONTH) && position > 0 ? TYPE_ITEM
+				: TYPE_GROUP_HEADER;
+
+	};
+
+	public TwitterListAdapter(Context ctx, List<TwitterObj> lst) {
+		twitterLst = lst;
+		mInflater = LayoutInflater.from(ctx);
+		formater = new SimpleDateFormat("EEEE MMMM dd", Locale.US);
 	}
+
+	@Override
+	public Filter getFilter() {
+		return filter;
+	}
+
+	public int getCount() {
+		return twitterLst == null ? 0 : twitterLst.size();
+	}
+
+	public Object getItem(int position) {
+		return twitterLst == null ? null : twitterLst.get(position);
+	}
+
+	public long getItemId(int position) {
+		return position;
+	}
+
+	public View getView(int position, View convertView, ViewGroup parent) {
+		
+		UiHolder holder;
+		
+		if (convertView == null)
+		{
+			holder = new UiHolder();
+			switch (getItemViewType(position)) {
+			case TYPE_ITEM:
+				convertView = mInflater.inflate(R.layout.list_item, null);
+				holder.imgPhoto = (ImageView) convertView.findViewById(R.id.img_photo);
+				holder.txtUser = (TextView) convertView.findViewById(R.id.txt_user);
+				holder.txtCreatedAt = (TextView) convertView.findViewById(R.id.txt_created_at);
+				holder.txtDescription = (TextView) convertView.findViewById(R.id.txt_description);
+				break;
+
+			case TYPE_GROUP_HEADER:
+				convertView = mInflater.inflate(R.layout.list_header, null);
+				holder.txtHeader = (TextView) convertView.findViewById(R.id.txt_header);
+				break;
+			}
+
+			convertView.setTag(holder);
+			
+		} else {
+			holder = (UiHolder) convertView.getTag();
+		}
+
+		switch (getItemViewType(position)) {
+		case TYPE_ITEM:
+			String url = twitterLst.get(position).getUser().getProfileImageURL();
+			new DownloadPhotoTask(holder.imgPhoto).execute(url);
+			holder.txtUser.setText(twitterLst.get(position).getUser().getName());
+			holder.txtCreatedAt.setText(twitterLst.get(position).getCreatedAt().getTime().toString());
+			holder.txtDescription.setText(twitterLst.get(position).getText());
+			break;
+
+		case TYPE_GROUP_HEADER:
+			holder.txtHeader.setText(formater.format(twitterLst.get(position).getCreatedAt().getTime()));
+			break;
+		}
+
+		return convertView;
+	}
+
+	static class UiHolder {
+		ImageView imgPhoto;
+		TextView txtUser;
+		TextView txtCreatedAt;
+		TextView txtDescription;
+		TextView txtHeader;
+	}
+
+	public class DownloadPhotoTask extends AsyncTask<String, Bitmap, Bitmap> {
+
+		private final WeakReference<ImageView> imgPhotoRef;
+
+		public DownloadPhotoTask(ImageView imgPhoto) {
+			imgPhotoRef = new WeakReference<ImageView>(imgPhoto);
+		}
+
+		@Override
+		protected void onPreExecute() {
+			setPhoto(null);
+			super.onPreExecute();
+		}
+
+		@Override
+		protected Bitmap doInBackground(String... url) {
+			URL urlPhoto = null;
+			Bitmap bmp = null;
+			try {
+				urlPhoto = new URL(url[0]);
+				bmp = BitmapFactory.decodeStream(urlPhoto.openConnection()
+						.getInputStream());
+			} catch (MalformedURLException e) {
+				Log.e(TAG, "Failed to get photo");
+			} catch (IOException e) {
+				Log.e(TAG, "Failed to get photo");
+			}
+			return bmp;
+		}
+
+		@Override
+		protected void onPostExecute(Bitmap bmp) {
+			setPhoto(bmp);
+
+		}
+
+		private void setPhoto(Bitmap bmp) {
+			final ImageView imgPhoto = imgPhotoRef.get();
+			if (imgPhoto != null) {
+				imgPhoto.setImageBitmap(bmp);
+			}
+		}
+	}
+
+}
